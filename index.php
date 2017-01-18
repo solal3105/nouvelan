f294228645f3fb34990419e4cd3562ffc2bf7b67
solal gendrin <adresse3105@hotmail.fr>
2017-01-18 16:48:07 +0100

    Revert "code php fini, place au design"
    This reverts commit 4c64d0813d307ecd33399e3dc6a34e3d7c92fef0.

--- index.php
+++ index.php
@@ -11,24 +11,21 @@
 	<main>
 		<h1>Résèrve ta bière en compagnie de la famille gendrin !</h1>
 
-
-		<form action="cible.php" method="post">
-			<label>Prénom :
-				<input name="prenom" type="text">
-			</label>
-			<label>Nom :
-				<input name="nom" type="text">
-			</label>
-			<label>Surnom (visible par tous) :
-				<input name="surnom" type="text">
-			</label>
-			<select name="type_de_boisson" size="1">
-				<option> Bière </option>
-				<option> Vin </option>
-				<option> Champagne </option>
-				<option> Eau </option>
-				<option> Café </option>
-			</select> <input type="submit">
+		<form action="cible.php" method="POST">
+			<p><label>Prénom :<input type="text" name="prenom" /></label></p>
+			<p><label>Nom :<input type="text" name="nom" /></label></p>
+			<p><label>Surnom (visible par tous) :<input type="text" name="surnom" /></label></p>
+			<p>
+				<select name="type_de_boisson" size="1">
+					<option>Bière</option>
+					<option>Vin</option>	
+					<option>Champagne</option>	
+					<option>Eau</option>
+					<option>Café</option>
+				</select>
+			</p>
+			<p><input type="submit"/></p>
+			
 		</form>
 	</main>
 </body>
